f3da28fd06303f0dc3b715f2d8603d44a7cb082e
LeonelTSW <leonel.zapata@tecdesoftware.edu.mx>
2025-11-05 11:49:20 -0600

    Ejercicio 9 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -143,8 +143,13 @@ function clasificarEdad(edad) {
  */
 function factorial(n) {
   // TODO: Implementa usando un bucle for
-  return 1;
-}
+  let resultado = 1;
+  for (let i = 1; i <= n; i++) {
+    resultado *= i;
+  }
+  return resultado;
+  } 
+
 
 /**
  * Ejercicio 3.2: Suma de nÃºmeros del 1 al n (4 puntos)
