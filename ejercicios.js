103969e7c3ca3282d931d81c6fe622a6be0ec8f6
LeonelTSW <leonel.zapata@tecdesoftware.edu.mx>
2025-11-11 23:18:08 -0600

    Ejercicio 14 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -230,7 +230,14 @@ function sumaArray(numeros) {
  */
 function promedioArray(numeros) {
   // TODO: Calcula el promedio (suma total / cantidad de elementos)
-  return 0;
+  let suma = 0;
+
+  for (let i = 0; i < numeros.length; i++) {
+    suma += numeros[i];
+  }
+
+  const promedio = suma / numeros.length;
+  return promedio;
 }
 
 /**
